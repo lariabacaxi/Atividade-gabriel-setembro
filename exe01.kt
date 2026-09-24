b43d405a257aaee96417c0cca4badac76cbe7a9b
Larocas1504 <lara.lopes.rosario@escola.pr.gov.br>
2026-09-24 11:15:01 -0300

    Add initial Kotlin program with print statements

---
+++ exe01.kt
@@ -0,0 +1,7 @@
+fu main() { 
+println("==========") 
+println("meu primeiro programa") 
+println("======") println("nome: aluno exemplo") 
+println("turma: 3ds") 
+println("escola: agv") println(" bem vindo ao kotlin!") 
+}
